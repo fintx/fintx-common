344598aef4a0f8c572abc0ac3a3fd2ebd9d4cb24
bluecreator <qiang.x.wang@gmail.com>
2017-08-15 23:54:13 +0800

    add arrays test class

---
+++ src/test/java/org/fintx/util/ArraysTest.java
@@ -0,0 +1,72 @@
+/**
+ *  Copyright 2017 FinTx
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fintx.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+
+/**
+ * @author bluecreator(qiang.x.wang@gmail.com)
+ *
+ */
+public class ArraysTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        Object[] ints=Arrays.subarray(new Object[10],2,3);
+        System.out.println(ints.length);
+        Strings.contains("abc   ","abc");
+    }
+    
+    
+    
+
+}
