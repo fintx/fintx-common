680e5adb6bf1cc42ef7f6f6f07c74663a93bd146
bluecreator <qiang.x.wang@gmail.com>
2017-08-24 23:57:18 +0800

    add Ips Test class and its referred class

---
+++ src/test/java/org/fintx/util/IpsTest.java
@@ -0,0 +1,51 @@
+/**
+ *  Copyright 2017 FinTx
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fintx.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+/**
+ * @author bluecreator(qiang.x.wang@gmail.com)
+ *
+ */
+public class IpsTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+
+    @Test
+    public void test() {
+        System.out.println(Ips.getLocalAddress());
+        Request request=new Request();
+        Ips.getRemoteAddress(request);
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Argument should not be null!");
+        Ips.getRemoteAddress(null);
+        
+    }
+
+}
